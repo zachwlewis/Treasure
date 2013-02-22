6fd5fa9884c4afed5302f0be2859b080d511bfa3
Zachary Lewis <zachary.lewis@thegamestudio.net>
2013-02-22 00:57:51 -0600

    Initial Commit
    - File Drag & Drop.
    - Basic filesystem.
    - Basic filetype highlighting.

---
+++ Treasure/MainWindow.xaml.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace Treasure
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		private readonly object _dummyNode = null;
+		private string _rootLocation = "";
+
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void TreasureMainWindow_Loaded(object sender, RoutedEventArgs e)
+		{
+		}
+
+		private void PopulateDirectoryList(string targetDirectory)
+		{
+			DirectoryList.Items.Clear();
+
+			TreeViewItem root = new TreeViewItem();
+			root.Header = targetDirectory;
+			root.Tag = targetDirectory;
+			root.FontWeight = FontWeights.Bold;
+			DirectoryList.Items.Add(root);
+			root.IsExpanded = true;
+
+			foreach (string s in Directory.GetDirectories(targetDirectory))
+			{
+				TreeViewItem item = new TreeViewItem();
+				item.Header = s.Substring(s.LastIndexOf("\\") + 1); ;
+				item.Tag = s;
+				item.FontWeight = FontWeights.Normal;
+				
+				// Is this item hidden?
+				if (item.Header.ToString().StartsWith(".", false, System.Globalization.CultureInfo.CurrentCulture))
+				{
+					item.FontStyle = FontStyles.Italic;
+					item.Foreground = new SolidColorBrush(Color.FromRgb(164, 195, 255));
+				}
+
+				item.Items.Add(_dummyNode);
+				item.Expanded += new RoutedEventHandler(folder_Expanded);
+				root.Items.Add(item);
+			}
+
+			foreach (string s in Directory.GetFiles(targetDirectory))
+			{
+				TreeViewItem item = new TreeViewItem();
+				item.Header = s.Substring(s.LastIndexOf("\\") + 1);
+				item.Tag = s;
+				item.FontWeight = FontWeights.Normal;
+				// Is this item hidden?
+				if (item.Header.ToString().StartsWith(".", false, System.Globalization.CultureInfo.CurrentCulture))
+				{
+					item.FontStyle = FontStyles.Italic;
+					item.Foreground = new SolidColorBrush(Color.FromRgb(164, 195, 255));
+				}
+				root.Items.Add(item);
+			}
+
+			
+		}
+
+		private void folder_Expanded(object sender, RoutedEventArgs e)
+		{
+			TreeViewItem item = (TreeViewItem) sender;
+			if (item.Items.Count == 1 && item.Items[0] == _dummyNode)
+			{
+				item.Items.Clear();
+
+				try
+				{
+					// Add all directories first.
+					foreach (string s in Directory.GetDirectories(item.Tag.ToString()))
+					{
+						TreeViewItem subitem = new TreeViewItem();
+						subitem.Header = s.Substring(s.LastIndexOf("\\") + 1);
+						subitem.Tag = s;
+						subitem.FontWeight = FontWeights.Normal;
+						
+						// Is this item hidden?
+						if (subitem.Header.ToString().StartsWith(".", false, System.Globalization.CultureInfo.CurrentCulture))
+						{
+							subitem.FontStyle = FontStyles.Italic;
+							subitem.Foreground = new SolidColorBrush(Color.FromRgb(164, 195, 255));
+						}
+
+						subitem.Items.Add(_dummyNode);
+						subitem.Expanded += new RoutedEventHandler(folder_Expanded);
+						item.Items.Add(subitem);
+					}
+
+					// Add all files second.
+					foreach (string s in Directory.GetFiles(item.Tag.ToString()))
+					{
+						TreeViewItem subitem = new TreeViewItem();
+						subitem.Header = s.Substring(s.LastIndexOf("\\") + 1);
+						subitem.Tag = s;
+						subitem.FontWeight = FontWeights.Normal;
+						
+						// Is this item hidden?
+						if (subitem.Header.ToString().StartsWith(".", false, System.Globalization.CultureInfo.CurrentCulture))
+						{
+							subitem.FontStyle = FontStyles.Italic;
+							subitem.Foreground = new SolidColorBrush(Color.FromRgb(164, 195, 255));
+						}
+
+						item.Items.Add(subitem);
+					}
+				}
+				catch (Exception) { }
+			}
+		}
+
+		private void DropTarget_DragEnter(object sender, DragEventArgs e)
+		{
+			if (e.Data.GetDataPresent(DataFormats.FileDrop))
+			{
+				e.Effects = DragDropEffects.Link;
+				Label target = (Label)sender;
+				target.Background = new SolidColorBrush(Color.FromRgb(164, 195, 255));
+			}
+			else { e.Effects = DragDropEffects.None; }
+		}
+
+		private void DropTarget_DragLeave(object sender, DragEventArgs e)
+		{
+			Label target = (Label)sender;
+			target.Background = new SolidColorBrush(Color.FromArgb(127, 164, 195, 255));
+		}
+
+		private void DropTarget_Drop(object sender, DragEventArgs e)
+		{
+			if (e.Data.GetDataPresent(DataFormats.FileDrop))
+			{
+				string[] dropData = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+				if (dropData.Length == 1) {
+					_rootLocation = dropData[0];
+					PopulateDirectoryList(_rootLocation);
+					Label target = (Label)sender;
+					target.Visibility = Visibility.Collapsed;
+					target.Background = new SolidColorBrush(Color.FromArgb(127, 164, 195, 255));
+				}
+				else
+				{
+					// TODO: Handle invalid drop.
+				}
+			}
+				
+
+			
+		}
+
+		private void TreasureMainWindow_DragLeave(object sender, DragEventArgs e)
+		{
+			if (_rootLocation != "") { DropTarget.Visibility = Visibility.Collapsed; }
+		}
+
+		private void TreasureMainWindow_DragEnter(object sender, DragEventArgs e)
+		{
+			if (e.Data.GetDataPresent(DataFormats.FileDrop)) { DropTarget.Visibility = Visibility.Visible; }
+		}
+	}
+}
